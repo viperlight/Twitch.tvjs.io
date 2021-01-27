247a22a0e5660cb14f294ae64afb9bc61aa94ab2
Wilbert-mad <angelbecerrah548@gmail.com>
2021-01-26 18:25:08 -0600

    first commit #1

---
+++ precache-manifest.c3790566da47f06bd60c15fd9e3476ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c79563877b15c4e46ec2fa152bcaba",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "d0226ea71fc00f1cbb29",
+    "url": "/css/app.d8750367.css"
+  },
+  {
+    "revision": "4d517e10c5e871212cd1f8a5d48ff348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0226ea71fc00f1cbb29",
+    "url": "/js/app.77f44ddd.js"
+  },
+  {
+    "revision": "7c7746a73e58675fcfe3",
+    "url": "/js/chunk-vendors.c5eac9ac.js"
+  },
+  {
+    "revision": "4f4fa7b59273d07a56b81028240f1f9c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0b69ffd26932c8427633437f3f565b32",
+    "url": "/static/logo-square.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.c3790566da47f06bd60c15fd9e3476ad.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "discord.js-website"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
